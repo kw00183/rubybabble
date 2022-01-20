074e03be9174fc8a1b81276e5008c0d4f1fdee0d
kw00183 <kw00183@cs6242-vm07.uwg.westga.edu>
2022-01-19 21:19:50 -0500

    create tile_group.rb file with TileGroup class containing methods to append tiles, remove tiles and create a hand of tiles (a string of all the letters)

---
+++ tile_group.rb
@@ -0,0 +1,28 @@
+class TileGroup
+  attr_accessor :tiles
+
+  def initialize
+    @tile_group = []
+  end
+
+  def tiles
+    return @tile_group
+  end
+
+  def append(tile)
+    @tile_group.push tile.to_sym
+  end
+
+  def remove(tile)
+    @index = @tile_group.find_index(tile.to_sym)
+    @tile_group.delete_at[@index]
+  end
+
+  def hand
+    @hand_array = []
+    @tile_group.each { |tile| @hand_array.push(tile.to_s) }
+    @hand = @hand_array.join("")
+    return @hand
+  end
+
+end
