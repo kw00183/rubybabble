79cf29f122a95f3c1bb48a21aeeaf651e565e49a
kw00183 <kw00183@cs6242-vm07.uwg.westga.edu>
2022-01-19 22:53:05 -0500

    update the initialize method of Word class

--- word.rb
+++ word.rb
@@ -3,10 +3,10 @@ require_relative "../../tile_bag.rb"
 class Word < TileGroup
 
   def initialize
-    @score = 0
   end
 
   def score
+    @score = 0
     @tiles_to_score = TileGroup.tiles
     @tiles_to_score.each {
       @score += |tile| TileBag.points_for(tile.to_sym)
