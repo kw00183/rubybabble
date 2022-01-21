3c87c17e8890b09672fc90d3c10e483c34d0fbf1
kw00183 <kw00183@cs6242-vm06.uwg.westga.edu>
2022-01-21 09:31:11 -0500

    add in final tests to the test_score.rb file containing the Word class tests

--- tests/word/test_score.rb
+++ tests/word/test_score.rb
@@ -15,10 +15,28 @@ class Word::TestInitialize < Minitest::Test
     assert_equal 0, @wd.score
   end
 
-  # test to return a score of 0 for empty word
+  # test to return a correct score for a single tile
   def test_score_a_one_tile_word
     @wd.append('A')
     assert_equal 1, @wd.score
+  end
+
+  # test to return a correct score for multiple tiles
+  def test_score_a_word_with_multiple_different_tiles
+    @wd.append('A')
+    @wd.append('R')
+    @wd.append('M')
+    @wd.append('S')
+    assert_equal 6, @wd.score
+  end
+
+  # test to return a correct score for word with recurring tiles
+  def test_score_a_word_with_recurring_tiles
+    @wd.append('F')
+    @wd.append('O')
+    @wd.append('O')
+    @wd.append('D')
+    assert_equal 12, @wd.score
   end
 
 end
